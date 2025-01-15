7221ba13f98f69d878dee547c8d56efb2ffe32e3
Calandarov <calandarov@icloud.com>
2025-01-15 21:20:25 +0300

    добавил логирование в фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,16 @@ import pytest
 
 from dotenv import dotenv_values
 
+from utils.logger.logger import file_logger
+
+
+log = file_logger(__name__)
+
 
 @pytest.fixture(scope="session")
 def env_config():
     env_path = ".env.local"
     try:
         return dotenv_values(env_path)
-    except Exception:
-        pass
+    except Exception as e:
+        log.error(e)
--- tests/test_api/conftest.py
+++ tests/test_api/conftest.py
@@ -2,10 +2,15 @@ import pytest
 
 from tests.clients.api.api_client import ApiClient
 
+from utils.logger.logger import file_logger
+
+
+log = file_logger(__name__)
+
 
 @pytest.fixture(scope="session")
 def api_client(env_config):
     try:
         return ApiClient(host=env_config["HOST"])
-    except Exception:
-        pass
+    except Exception as e:
+        log.error(e)
